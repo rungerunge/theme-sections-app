a59fcc29e0de4ae233453ed7b073cd27e8b1d1d6
Silas Nielsen <silasnielsen@Silass-MacBook-Pro.local>
2025-04-15 11:21:19 +0200

    Update build configuration to use web directory

---
+++ web/remix.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  ignoredRouteFiles: ["**/.*"],
+  server: "../server.js",
+  serverBuildPath: "../build/index.js",
+  serverModuleFormat: "cjs",
+  appDirectory: "app",
+  assetsBuildDirectory: "../public/build",
+  publicPath: "/build/",
+  serverDependenciesToBundle: ["@shopify/shopify-app-remix", /@shopify\/shopify-api.*/],
+}; 
